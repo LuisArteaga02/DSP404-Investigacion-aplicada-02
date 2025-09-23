7a47e4e088f2dce40559fce59bc9a7bc320918b6
B3atrriz <elisabetgonzalez2007@gmail.com>
2025-09-23 15:53:04 -0600

    Subir proyecto BibliotecaWeb completo

---
+++ Models/Usuario.cs
@@ -0,0 +1,21 @@
+﻿namespace BibliotecaWeb.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public required string Nombre { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
+
+        public DateTime FechaRegistro { get; set; } = DateTime.Now;
+
+        public ICollection<Prestamo> Prestamos { get; set; } = new List<Prestamo>();
+    }
+}
